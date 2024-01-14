c933b7d69e4b258c99dfc864b9264673f987f060
David Cantu <davidcantu966@gmail.com>
2024-01-14 14:32:16 -0600

    feat: i18n validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   validates :name, presence: true
-  validates :domain, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\.[a-zA-Z]{2,}\z/, message: "must be a valid domain" }
+  validates :domain, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\.[a-zA-Z]{2,}\z/ }
+  validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
 
   def email_of_company_domain
     if Company.find_by(domain: email.split('@').last).nil?
-      errors.add(:email, "correo debe ser de un dominio de empresa válido")
+      errors.add(:email, :invalid_domain)
     end
   end
 end
