2a27e4635cff6dc3425046d827d01b6a0fc2e904
David Cantu <davidcantu966@gmail.com>
2024-02-24 14:00:26 -0600

    Add associations for term offerings and terms in Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+  has_many :term_offerings
+  has_many :terms, through: :term_offerings
   validates :name, presence: true
   validates :domain, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\.[a-zA-Z]{2,}\z/ }
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
