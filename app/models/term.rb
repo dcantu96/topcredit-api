2414d8706b75b56e1ddbf68462affd727faee123
David Cantu <davidcantu966@gmail.com>
2024-02-24 18:59:21 -0600

    Add duration validation and update column name in Term model form type to duration type

--- app/models/term.rb
+++ app/models/term.rb
@@ -3,5 +3,6 @@ class Term < ApplicationRecord
   has_many :companies, through: :term_offerings
 
   validates :name, presence: true
-  validates_inclusion_of :type, in: %w( biweekly monthly yearly )
+  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates_inclusion_of :duration_type, in: %w( biweekly monthly yearly )
 end
--- db/migrate/20240223191438_create_terms.rb
+++ db/migrate/20240223191438_create_terms.rb
@@ -1,13 +1,13 @@
 class CreateTerms < ActiveRecord::Migration[7.1]
   def change
     create_table :terms do |t|
-      t.string :type
+      t.string :duration_type
       t.integer :duration
       t.string :name
 
       t.timestamps
     end
     
-    add_index :terms, [:duration, :type], unique: true
+    add_index :terms, [:duration, :duration_type], unique: true
   end
 end
