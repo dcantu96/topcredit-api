117cc9bc97c3b85c40f828a0e5b76d15a9122b72
David Cantu <davidcantu966@gmail.com>
2024-02-24 23:52:25 -0600

    Remove presence validation for name field in Term model

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,7 +2,6 @@ class Term < ApplicationRecord
   has_many :term_offerings
   has_many :companies, through: :term_offerings
 
-  validates :name, presence: true
   validates :duration, presence: true, numericality: { greater_than: 0 }
   validates_inclusion_of :duration_type, in: %w( two-weeks months years )
 end
