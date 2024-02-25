b3735244727fe33f18fab3120e85a64916f3d83b
David Cantu <davidcantu966@gmail.com>
2024-02-24 19:00:57 -0600

    Remove 'terms' attribute and add 'term_offerings' association to Api::CompanyResource

--- app/resources/api/company_resource.rb
+++ app/resources/api/company_resource.rb
@@ -1,3 +1,5 @@
 class Api::CompanyResource < JSONAPI::Resource
-  attributes :name, :domain, :terms, :rate, :created_at, :updated_at
+  attributes :name, :domain, :rate, :created_at, :updated_at
+  has_many :term_offerings
+  has_many :terms, through: :term_offerings
 end
