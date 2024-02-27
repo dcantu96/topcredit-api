15df3c8da8c081c050cad720e52d02a7b77c6f2f
David Cantu <davidcantu966@gmail.com>
2024-02-27 15:09:43 -0600

    feat: add status filters for credit resource

--- app/resources/api/credit_resource.rb
+++ app/resources/api/credit_resource.rb
@@ -2,4 +2,6 @@ class Api::CreditResource < JSONAPI::Resource
   attributes :status, :loan, :created_at, :updated_at
   has_one :borrower, foreign_key: 'user_id', class_name: 'User'
   has_one :term
+
+  filter :status
 end
