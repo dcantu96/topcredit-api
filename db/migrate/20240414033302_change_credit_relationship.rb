10eb12d2477f969e45d44dee9870415767756527
David Cantu <davidcantu966@gmail.com>
2024-04-14 09:20:28 -0600

    fix: adjust migration

--- db/migrate/20240414033302_change_credit_relationship.rb
+++ db/migrate/20240414033302_change_credit_relationship.rb
@@ -3,9 +3,7 @@ class ChangeCreditRelationship < ActiveRecord::Migration[7.1]
     # Add the new column without NOT NULL constraint initially
     add_reference :credits, :term_offering, foreign_key: true
 
-    # Temporary disable the foreign key constraint during data update
-    # Adjust the table name `term_offerings` as necessary based on your actual table name
-    execute "ALTER TABLE credits DISABLE TRIGGER ALL;"
+    Credit.reset_column_information # Ensure the model is up-to-date
 
     # Update the term_offering_id based on associated user data
     Credit.includes(:borrower).find_each do |credit|
@@ -16,9 +14,6 @@ class ChangeCreditRelationship < ActiveRecord::Migration[7.1]
         credit.update(term_offering_id: term_offering.id) if term_offering
       end
     end
-
-    # Re-enable foreign key constraints
-    execute "ALTER TABLE credits ENABLE TRIGGER ALL;"
 
     # Change the column to NOT NULL only after all data has been updated
     change_column :credits, :term_offering_id, :bigint, null: false
@@ -32,17 +27,13 @@ class ChangeCreditRelationship < ActiveRecord::Migration[7.1]
     add_reference :credits, :term, foreign_key: true
 
     # Temporarily disable the foreign key constraint during data restoration
-    execute "ALTER TABLE credits DISABLE TRIGGER ALL;"
-
+    Credit.reset_column_information # Ensure the model is up-to-date
     # Restore term_id from term_offering_id
     Credit.includes(:term_offering).find_each do |credit|
       if credit.term_offering
         credit.update_column(:term_id, credit.term_offering.term_id) # using update_column to avoid callbacks and validations
       end
     end
-
-    # Re-enable foreign key constraints
-    execute "ALTER TABLE credits ENABLE TRIGGER ALL;"
 
     # Remove the term_offering reference column
     remove_reference :credits, :term_offering, foreign_key: true, null: false
