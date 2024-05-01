313411654ad1aa8a1da31f5ce12a8cb3947d88f3
David Cantu <davidcantu966@gmail.com>
2024-05-01 14:10:28 -0600

    feat: new enable current user for json api context

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,4 +4,10 @@ class Api::ApiController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def context
+    current_user =
+      User.includes(:roles).find(doorkeeper_token.resource_owner_id)
+    { current_user: current_user }
+  end
 end
