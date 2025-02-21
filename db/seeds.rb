cf54f4cafcc215000081f54bb6a31368b90ce781
David Cantu <davidcantu966@gmail.com>
2025-02-21 17:23:05 -0600

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,7 @@ if Doorkeeper::Application.count.zero?
 end
 
 # To get the client_id and client_secret for the application, run the following commands in the Rails console:
-# Doorkeeper::Application.first.uid
-# Doorkeeper::Application.first.secret
+# puts "uid: #{Doorkeeper::Application.first.uid} \nsecret: #{Doorkeeper::Application.first.secret}"
 
 create(:user, :admin, email: "admin@staff.com")
 
@@ -26,43 +25,51 @@ create_list(:term, 4, :monthly)
 # Create users.
 company = create(:company, term_count: 2)
 10.times do
+  create(:credit, :dispersed, term_offering: company.term_offerings.sample)
+end
+5.times do
   create(
     :credit,
-    :with_documents,
-    :dispersed,
-    :hr_approved,
+    :with_missing_payments,
     term_offering: company.term_offerings.sample
   )
+end
+5.times do
+  create(:credit, :defaulted, term_offering: company.term_offerings.sample)
 end
 company = create(:company, term_count: 3)
 20.times do
+  create(:credit, :dispersed, term_offering: company.term_offerings.sample)
+end
+5.times do
   create(
     :credit,
-    :with_documents,
-    :dispersed,
-    :hr_approved,
+    :with_missing_payments,
     term_offering: company.term_offerings.sample
   )
 end
-company = create(:company, term_count: 3)
-100.times do
-  create(
-    :credit,
-    :with_documents,
-    :dispersed,
-    :hr_approved,
-    term_offering: company.term_offerings.sample
-  )
+5.times do
+  create(:credit, :defaulted, term_offering: company.term_offerings.sample)
 end
-company = create(:company, term_count: 3)
-400.times do
-  create(
-    :credit,
-    :with_documents,
-    :dispersed,
-    :hr_approved,
-    term_offering: company.term_offerings.sample
-  )
-end
+# company = create(:company, term_count: 3)
+# 100.times do
+#   create(
+#     :credit,
+#     :with_documents,
+#     :dispersed,
+#     :hr_approved,
+#     term_offering: company.term_offerings.sample
+#   )
+# end
+# company = create(:company, term_count: 3)
+# 400.times do
+#   create(
+#     :credit,
+#     :with_documents,
+#     :dispersed,
+#     :hr_approved,
+#     term_offering: company.term_offerings.sample
+#   )
+# end
 
 puts "Seeds finished"
